f3b068650d2638626b2bd95b4e183c71857fd418
RapidMindd <YaroslavT007@yandex.ru>
2026-09-18 20:34:15 +0300

    thread join fixed

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ CXX = g++
 CXXFLAGS = -std=c++20 -Wall -Wextra -O2
 SRC = main.cpp
 OUT = a.out
-ARGS ?= 2.0 1000 10
+ARGS ?= 2.0 1'000'000 10
 
 .PHONY: run bench clean
 
--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
-#include <random>
 #include <pthread.h>
+#include <random>
 
 struct Californication {
   double r_;
@@ -8,14 +8,11 @@ struct Californication {
   size_t seed_;
 };
 
-bool isInside(double x, double y, double r)
-{
-  return x * x + y * y <= r * r;
-}
+bool isInside(double x, double y, double r) { return x * x + y * y <= r * r; }
 
 void* calc(void* input /* double r, size_t tests, size_t seed */)
 {
-  Californication data = *static_cast< Californication* >(input);
+  Californication data = *static_cast<Californication*>(input);
   std::mt19937 gen(data.seed_);
   std::uniform_real_distribution<> distribution(-data.r_, data.r_);
   size_t successful = 0;
@@ -28,35 +25,39 @@ void* calc(void* input /* double r, size_t tests, size_t seed */)
 double getArea(double r, size_t threads, size_t tests)
 {
   size_t tests_per_thread = tests / threads;
-  std::vector< pthread_t > descriptors(threads);
-  std::vector< Californication > data(threads);
-  auto DRY = [=, &descriptors, &data](size_t i, size_t tests_count)
-  {
+  std::vector<pthread_t> descriptors(threads);
+  std::vector<Californication> data(threads);
+  size_t created = 0;
+  auto DRY = [=, &descriptors, &data, &created](size_t i, size_t tests_count) {
     data[i] = {r, tests_count, i};
     int err = pthread_create(&descriptors[i], nullptr, calc, &data[i]);
     if (err) {
       std::cerr << strerror(err) << '\n';
       return 0;
     }
+    ++created;
     return 1;
   };
-  for (size_t i = 0; i < threads - 1; ++i) {
-    if (!DRY(i, tests_per_thread)) {
-      return -1.0;
-    }
+  bool failed = false;
+  for (size_t i = 0; i < threads - 1 && !failed; ++i) {
+    failed = !DRY(i, tests_per_thread);
   }
-  if (!DRY(threads - 1, tests % threads + tests_per_thread)) {
-    return -1.0;
+  if (!failed) {
+    failed = !DRY(threads - 1, tests % threads + tests_per_thread);
   }
   double successful = 0;
-  for (size_t i = 0; i < threads; ++i) {
+  for (size_t i = 0; i < created; ++i) {
     size_t* code = 0;
-    int err = pthread_join(descriptors[i], reinterpret_cast< void** >(&code));
+    int err = pthread_join(descriptors[i], reinterpret_cast<void**>(&code));
     if (err) {
       std::cerr << strerror(err) << '\n';
-      return -1;
+      failed = true;
+      continue;
     }
-    successful += reinterpret_cast< size_t>(code);
+    successful += reinterpret_cast<size_t>(code);
+  }
+  if (failed) {
+    return -1.0;
   }
   return successful / tests * r * r * 4;
 }
